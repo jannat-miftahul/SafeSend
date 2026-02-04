6e1f197c4272f9a3e69e9d0f333fbe8fb2211a30
jannat-miftahul <jannat27120@gmail.com>
2026-02-05 00:12:45 +0600

    Implement core Flask application for secure file upload and retrieval

---
+++ src/web-application/app.py
@@ -0,0 +1,118 @@
+import os
+import os.path
+from flask import Flask, request, redirect, url_for, render_template, session, send_from_directory, send_file
+from werkzeug.utils import secure_filename
+import DH
+import pickle
+import random
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, 'media', 'text-files')
+UPLOAD_KEY = os.path.join(BASE_DIR, 'media', 'public-keys')
+ALLOWED_EXTENSIONS = set(['txt'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+	return '.' in filename and \
+		filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+'''
+-----------------------------------------------------------
+					PAGE REDIRECTS
+-----------------------------------------------------------
+'''
+def post_upload_redirect():
+	return render_template('post-upload.html')
+
+@app.route('/register')
+def call_page_register_user():
+	return render_template('register.html')
+
+@app.route('/home')
+def back_home():
+	return render_template('index.html')
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/upload-file')
+def call_page_upload():
+	return render_template('upload.html')
+'''
+-----------------------------------------------------------
+				DOWNLOAD KEY-FILE
+-----------------------------------------------------------
+'''
+@app.route('/public-key-directory/retrieve/key/<username>')
+def download_public_key(username):
+	for root,dirs,files in os.walk(os.path.join(BASE_DIR, 'media', 'public-keys')):
+		for file in files:
+			file_parts = file.split('-')
+			if file_parts[0] == username:
+				filename = os.path.join(UPLOAD_KEY, file)
+				return send_file(filename, download_name='publicKey.pem',as_attachment=True)
+	return "Public Key not found", 404
+
+@app.route('/file-directory/retrieve/file/<filename>')
+def download_file(filename):
+	filepath = os.path.join(UPLOAD_FOLDER, filename)
+	if(os.path.isfile(filepath)):
+		return send_file(filepath, download_name='fileMessage-SafeSendSecurity.txt',as_attachment=True)
+	else:
+		return render_template('file-list.html',msg='An issue encountered, our team is working on that')
+
+'''
+-----------------------------------------------------------
+		BUILD - DISPLAY FILE - KEY DIRECTORY
+-----------------------------------------------------------
+'''
+# Build public key directory
+@app.route('/public-key-directory/')
+def downloads_pk():
+	username = []
+	if(os.path.isfile(os.path.join(BASE_DIR, "media", "database", "database_1.pickle"))):
+		pickleObj = open(os.path.join(BASE_DIR, "media", "database", "database_1.pickle"),"rb")
+		username = pickle.load(pickleObj)
+		pickleObj.close()
+	if len(username) == 0:
+		return render_template('public-key-list.html',msg='Aww snap! No public key found in the database')
+	else:
+		return render_template('public-key-list.html',msg='',itr = 0, length = len(username),directory=username)
+
+# Build file directory
+@app.route('/file-directory/')
+def download_f():
+	for root,dirs,files in os.walk(UPLOAD_FOLDER):
+		if(len(files) == 0):
+			return render_template('file-list.html',msg='Aww snap! No file found in directory')
+		else:
+			return render_template('file-list.html',msg='',itr=0,length=len(files),list=files)
+	return render_template('file-list.html',msg='Aww snap! No file found in directory')
+
+'''
+-----------------------------------------------------------
+				UPLOAD ENCRYPTED FILE
+-----------------------------------------------------------
+'''
+
+@app.route('/data', methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit a empty part without filename
+		if file.filename == '':
+			flash('No selected file')
+			return 'NO FILE SELECTED'
+		if file:
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+			return post_upload_redirect()
+		return 'Invalid File Format !'
